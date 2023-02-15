a038e7c593f3892e8b83348292f6f6ab58fdbc4f
Hirendra77 <119104906+Hirendra77@users.noreply.github.com>
2023-02-15 18:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ const getUserButton = document.getElementById("getUser");
 const name=document.getElementById("Name");
 const photo = document.getElementById("photo");
 const additionalInfo = document.getElementById("additionalInfo");
- getUserButton.addEventListener("click", async function(){
+ getUserButton.addEventListener("click", function(){
 
-	 const response = await fetch("https://randomuser.me/api/");
-	 if(response){
-		 const data = await response.json();
-		 const user = data.results[0];
-		  name.innerText=user.name.first+" "+user.name.last;
-		 photo.src= user.picture.large;
-		 name.dataset.age = user.dob.age;
-		 name.dataset.email = user.email;
-		 name.dataset.phone = user.phone;
-		 additionalInfo.innerText = "";
-	 }
-	 // fetch("https://randomuser.me/api/")
-	 // .then(response=>response.json())
-	 // .then(data=>{
+	 // const response = await fetch("https://randomuser.me/api/");
+	 // if(response){
+		//  const data = await response.json();
 		//  const user = data.results[0];
-		//  name.innerText=user.name.first+" "+user.name.last;
+		//   name.innerText=user.name.first+" "+user.name.last;
 		//  photo.src= user.picture.large;
 		//  name.dataset.age = user.dob.age;
 		//  name.dataset.email = user.email;
 		//  name.dataset.phone = user.phone;
 		//  additionalInfo.innerText = "";
-	 // });
+	 // }
+	 fetch("https://randomuser.me/api/")
+	 .then(response=>response.json())
+	 .then(data=>{
+		 const user = data.results[0];
+		 name.innerText=user.name.first+" "+user.name.last;
+		 photo.src= user.picture.large;
+		 name.dataset.age = user.dob.age;
+		 name.dataset.email = user.email;
+		 name.dataset.phone = user.phone;
+		 additionalInfo.innerText = "";
+	 });
  });
 const ageButton = document.querySelector('[data-attr ="age"]');
 const emailButton = document.querySelector('[data-attr ="email"]');
